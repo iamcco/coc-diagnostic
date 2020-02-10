957f3b3a55aefc9ffc2b38a95c42e8477e7118f4
yaegassy <yaegassy@users.noreply.github.com>
2020-02-10 15:38:17 +0900

    Add phpstan support

--- src/config.ts
+++ src/config.ts
@@ -284,6 +284,24 @@ export const linters = {
       "message": "${Text} [${FromLinter}]",
     },
   },
+
+  "phpstan": {
+    "command": "./vendor/bin/phpstan",
+    "debounce": 100,
+    "rootPatterns": [ "composer.json", "composer.lock", "vendor", ".git" ],
+    "args": [ "analyze", "--error-format", "raw", "--no-progress", "%file" ],
+    "offsetLine": 0,
+    "offsetColumn": 0,
+    "sourceName": "phpstan",
+    "formatLines": 1,
+    "formatPattern": [
+      "^[^:]+:(\\d+):(.*)$",
+      {
+        "line": 1,
+        "message": 2,
+      },
+    ],
+  },
 }
 
 export const formatters = {
