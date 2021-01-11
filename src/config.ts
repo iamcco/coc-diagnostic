1b87f6a683f893a0105570d06a0cdca43f67a71c
javiertury <javiertury@gmail.com>
2021-01-11 20:45:10 +0100

    feat(config): add flake8
    Add support for flake8. There is already support for pylint which is more complete but flake8 is faster and more extensible.

--- src/config.ts
+++ src/config.ts
@@ -582,7 +582,34 @@ export const linters = {
       "2": "error",
       "1": "warning"
     }
+  },
+
+  "flake8": {
+    "command": "flake8",
+    "debounce": 100,
+    "args": [ "--format=%(row)d,%(col)d,%(code).1s,%(code)s: %(text)s", "-" ],
+    "offsetLine": 0,
+    "offsetColumn": 0,
+    "sourceName": "flake8",
+    "formatLines": 1,
+    "formatPattern": [
+      "(\\d+),(\\d+),([A-Z]),(.*)(\\r|\\n)*$",
+      {
+        "line": 1,
+        "column": 2,
+        "security": 3,
+        "message": 4
+      }
+    ],
+    "securities": {
+      "W": "warning",
+      "E": "error",
+      "F": "error",
+      "C": "error",
+      "N": "error"
+    }
   }
+
 }
 
 export const formatters = {
